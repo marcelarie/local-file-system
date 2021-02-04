14316371b5b42cec375616f4947982d50f795c48
Marcel <googlillo@gmail.com>
2021-02-04 20:07:51 +0100

    Add date filter and style to file rows

--- src/js/components/files/files.js
+++ src/js/components/files/files.js
@@ -5,10 +5,11 @@ const file = {
     renderFile: function (files) {
         const template = `
                     <div class="files">
-                        <p>${files.name}</p>
-                        <p>${files.date}</p>
-                        <p>${files.size}</p>
-                        <p>${files.type}</p>
+                        <p class="files__row files__row-name" id='${files.name}'>${files.name}</p>
+                        <p class="files__row files__row-date" id='${files.name}'>${files.date}</p>
+                        <p class="files__row files__row-date" id='${files.name}'>${files.lastDate}</p>
+                        <p class="files__row files__row-size" id='${files.name}'>${files.size}</p>
+                        <p class="files__row files__row-type" id='${files.name}'>${files.type}</p>
                     </div>`
         if (!files.dir) {
             render.renderComponent(template, 'all-files__selector')
