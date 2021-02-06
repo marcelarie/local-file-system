18069f5381c2814fa7017eac82af2d456494458d
Marcel <googlillo@gmail.com>
2021-02-06 21:38:39 +0100

    Add file path to html components.

--- src/php/filter-type.php
+++ src/php/filter-type.php
@@ -6,19 +6,19 @@ require 'extract-file-data.php';
 function filterType($path)
 {
     $data = extractFileData($path);
+
+    $filePath = explode('[', $path)[0];
     
     $fileName = getFileName($path, $data);
 
     //type
     switch ($data[0]) {
             case 'dir':                             //date    lastMod    size
-                $dir = new Folder(end($fileName), $data[1], $data[2], $data[4], $path);
-                // if (end($fileName) !== 'root' && end($fileName) !== '.') {
+                $dir = new Folder(end($fileName), $data[1], $data[2], $data[4], $filePath);
                     return $dir;
-                // }
             break;
             case 'file':                      //date  lastMod   extension   size
-                $file = new File(end($fileName), $data[1], $data[2], $data[3], $data[4], $path);
+                $file = new File(end($fileName), $data[1], $data[2], $data[3], $data[4], $filePath);
                 return $file;
             break;
         }
