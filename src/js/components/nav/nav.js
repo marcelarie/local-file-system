c0afe740435dbeb0ab88360fff4efcfe0a6681bf
Marcel <googlillo@gmail.com>
2021-02-08 21:28:26 +0100

    Made search bar work case insensitive

--- src/js/components/nav/nav.js
+++ src/js/components/nav/nav.js
@@ -14,15 +14,28 @@ const nav = {
     searchListener: function () {
         const searchBar = document.getElementById('nav__items-search')
 
-
-
         searchBar.addEventListener('input', e => {
+            console.log(e.inputType)
             if (searchBar.value) {
                 allFiles.showAllFiles('show');
+                this.filterResults(searchBar.value)
             } else {
                 allFiles.showAllFiles('hide');
             }
         })
+    },
+    filterResults: function (value) {
+        const allResults = document.getElementById('all-files__selector').children
+        if (value) {
+            for (let result of allResults) {
+                result.classList.add('none')
+                const fileName = result.children[1].textContent.toUpperCase();
+                if (fileName.includes(value.toUpperCase())) {
+                    result.classList.remove('none')
+                }
+            }
+        }
+
     }
 }
 
