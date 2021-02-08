3529e8fbb7b6218015af53332ef5eb0f42b1538c
Marcel <googlillo@gmail.com>
2021-02-08 12:06:01 +0100

    Add styles to folders_box

--- src/js/components/recent-folders/recent-folders.js
+++ src/js/components/recent-folders/recent-folders.js
@@ -4,14 +4,14 @@ const recentFolders = {
     template: `
         <div class="recent-folders" id="recent-folders">
             <h3 class="recent-folders__title">Recent</h3>
-            <div class="recent-folders__selector" id="recent-folders__selector">
+            <div class="recent-folders__selector overflowXscroll" id="recent-folders__selector">
             </div>
         </div>
     `,
     recentFoldersListener: function () {
         const recentFolders = document.getElementById('recent-folders__selector');
         const allFiles = document.getElementById('all-files')
-        const sidebar = document.getElementById('sidebar');
+        // const sidebar = document.getElementById('sidebar');
 
         recentFolders.addEventListener('click', e => {
             if (e.target && e.target.classList.contains('folders__box')) {
@@ -24,8 +24,12 @@ const recentFolders = {
                     if (insideBox.id !== e.target.id) {
                         // apply display none to other folders
                         folder.classList.add('none');
+                        e.target.classList.remove('max15vh', 'overflow-hidden');
+                        e.target.classList.add('scroll');
                         allFiles.classList.add('none');
                         // sidebar.classList.add('none');
+                        recentFolders.classList.remove('overflowXscroll')
+                        recentFolders.classList.add('height50vh')
 
                     }
                 }
